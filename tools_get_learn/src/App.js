19850040326778dcaaadedf0de5a9518af48e105
soumytiwari <tiwarisoumya111@gmail.com>
2024-02-24 01:16:15 +0530

    Update1

--- tools_get_learn/src/App.js
+++ tools_get_learn/src/App.js
@@ -1,22 +1,11 @@
 import "./App.css";
-import React, { useState } from "react";
+import React from "react";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 
 // props: the information that we would change when using, should not keep changing in the main component file.
 // Value defined while using it.
 function App() {
-  // state, here for callback to handle search operation using both navbar and textform .js
-  const [searchText, setSearchText] = useState("");
-  const [text, setText] = useState("");
-
-  const handleSearchTextChange = (newSearchText) => {
-    setSearchText(newSearchText);
-  };
-
-  const handleTextChange = (newText) => {
-    setText(newText);
-  };
 
   const appStyles = {
     backgroundColor: "Gray",
@@ -38,8 +27,6 @@ function App() {
             heading="Write here, write here <3"
             btn_cap="Click to convert to UpperCase"
             btn_low="Click to convert to LowerCase"
-            onSearchTextChange={handleSearchTextChange}
-            onTextChange={handleTextChange}       //  pass the callback to update text
           />
         </div>
       </div>
